755cecbdef1fb14e7b586485361f7cdb475a2ccc
Rishabh Agarwal <rishabhagarwal@google.com>
2019-03-02 06:09:01 +0000

    Fix TODO about removing transpose

--- model.py
+++ model.py
@@ -38,19 +38,16 @@ def VI_Block(X, S1, S2, config):
     # CHANGE TO THEANO ORDERING
     # Since we are selecting over channels, it becomes easier to work with
     # the tensor when it is in NCHW format vs NHWC
-    q = tf.transpose(q, perm=[0, 3, 1, 2])
 
     # Select the conv-net channels at the state position (S1,S2).
     # This intuitively corresponds to each channel representing an action, and the convnet the Q function.
     # The tricky thing is we want to select the same (S1,S2) position *for each* channel and for each sample
-    # TODO: performance can be improved here by substituting expensive
-    #       transpose calls with better indexing for gather_nd
     bs = tf.shape(q)[0]
     rprn = tf.reshape(tf.tile(tf.reshape(tf.range(bs), [-1, 1]), [1, state_batch_size]), [-1])
     ins1 = tf.cast(tf.reshape(S1, [-1]), tf.int32)
     ins2 = tf.cast(tf.reshape(S2, [-1]), tf.int32)
-    idx_in = tf.transpose(tf.stack([ins1, ins2, rprn]), [1, 0])
-    q_out = tf.gather_nd(tf.transpose(q, [2, 3, 0, 1]), idx_in, name="q_out")
+    idx_in = tf.transpose(tf.stack([rprn, ins1, ins2]), [1, 0])
+    q_out = tf.gather_nd(q, idx_in, name="q_out")
 
     # add logits
     logits = tf.matmul(q_out, w_o)
